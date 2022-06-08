2a2ff07c3f4fa2d50fa17b3ac157e73384d456a5
idansi98 <idansi98@gmail.com>
2022-06-08 16:48:20 +0300

    FireBase almost works

--- app/src/main/java/com/example/androidapp/LoginActivity.java
+++ app/src/main/java/com/example/androidapp/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.example.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidapp.classes.LoginRunner;
+
 public class LoginActivity extends AppCompatActivity {
     EditText userName, password;
-    Button loginButton;
+    Button loginButton, toRegisterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +20,36 @@ public class LoginActivity extends AppCompatActivity {
         userName = findViewById(R.id.editTextTextPersonName);
         password = findViewById(R.id.editTextTextPassword);
         loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        toRegisterButton = findViewById(R.id.toRegButton);
+        loginButton.setOnClickListener(v -> {
+            try {
+                login(userName.getText().toString(), password.getText().toString());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        toRegisterButton.setOnClickListener(v -> {
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        });
+
+        /*loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String UserName = userName.getText().toString();
                 String Password = password.getText().toString();
 
             }
-        });
+        });*/
+    }
+    private boolean login (String userName, String password) throws InterruptedException {
+        LoginRunner foo = new LoginRunner();
+        foo.setValues(userName, password);
+        Thread thread = new Thread(foo);
+        thread.start();
+        thread.join();
+        //int value = foo.getValue();
+        Log.d("Fetch_Login", "Here");
+        return foo.getValue();
     }
 }
--- app/src/main/java/com/example/androidapp/RegisterActivity.java
+++ app/src/main/java/com/example/androidapp/RegisterActivity.java
@@ -1,14 +1,20 @@
 package com.example.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+import java.util.Locale;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText userName, displayName, password, passwordValidator;
     Button registerButton;
+    List<String> stringList;
     /*
     FirebaseAuth auth;
     DatabaseReference ref;*/
@@ -22,5 +28,30 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.editTextTextPassword);
         passwordValidator = findViewById(R.id.editTextTextPasswordValidation);
         registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(v -> {
+            if (!valid()) {
+                return;
+            }
+            //try to add user.
+            boolean result = createUser(userName.getText().toString(), displayName.getText().toString(),
+                    password.getText().toString());
+
+            Intent i = new Intent(this, RegisterActivity.class);
+            //i.putExtra();
+            startActivity(i);
+        });
+    }
+    private boolean createUser (String userName, String displayName, String password) {
+        try {
+            return true;
+        }
+        catch (Exception e) {
+            Log.e("Fetch", e.getMessage().toLowerCase(Locale.ROOT));
+            return false;
+        }
+
+    }
+    private boolean valid () {
+        return true;
     }
 }

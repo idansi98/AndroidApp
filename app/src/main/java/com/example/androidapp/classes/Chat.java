87db2d06ef4eb0c1511385e747ad4699a59e853a
idansi98 <idansi98@gmail.com>
2022-06-09 22:56:38 +0300

    a

--- app/src/main/java/com/example/androidapp/classes/Chat.java
+++ app/src/main/java/com/example/androidapp/classes/Chat.java
@@ -3,29 +3,39 @@ package com.example.androidapp.classes;
 import java.util.ArrayList;
 
 public class Chat {
-    private ArrayList<Message> messages;
-    private int id;
+    private final String userName; // is the key but if doesn't work can use a new key field
+    private final String displayName;
+    private final String serverAdr;
+    private final ArrayList<Message> messages;
 
-    public ArrayList<Message> getMessages() {
-        return messages;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Chat(ArrayList<Message> messages, int id) {
-        this.messages = messages;
-        this.id = id;
+    public Chat(String userName, String displayName, String serverAdr) {
+        this.userName = userName;
+        this.displayName = displayName;
+        this.serverAdr = serverAdr;
+        this.messages = new ArrayList<>();
     }
     private void addMessage(Message message) {
         messages.add(message);
     }
 
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
     private Message getLastMessage() {
         if (this.messages.size() != 0) {
             return this.messages.get(this.messages.size() - 1);
         }
         return null;
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getServerAdr() {
+        return serverAdr;
     }
 }

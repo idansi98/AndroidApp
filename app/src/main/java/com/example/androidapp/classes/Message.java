43f5fa3bb8879834b8d29ff695a1e773f21ebece
River <tziony.i@gmail.com>
2022-06-09 19:04:12 +0300

    Edited some classes

--- app/src/main/java/com/example/androidapp/classes/Message.java
+++ app/src/main/java/com/example/androidapp/classes/Message.java
@@ -3,13 +3,26 @@ package com.example.androidapp.classes;
 import java.time.LocalDateTime;
 
 public class Message {
-    private String text;
-    private LocalDateTime localDateTime;
-    private boolean userSent;
+    private final String text;
+    private final LocalDateTime localDateTime;
+    private final boolean userSent;
 
     public Message(String text, LocalDateTime localDateTime, boolean userSent) {
         this.text = text;
         this.localDateTime = localDateTime;
         this.userSent = userSent;
     }
+
+    public String getText() {
+        return text;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public boolean isUserSent() {
+        return userSent;
+    }
+
 }
--- app/src/main/java/com/example/androidapp/classes/User.java
+++ app/src/main/java/com/example/androidapp/classes/User.java
@@ -3,27 +3,50 @@ package com.example.androidapp.classes;
 import java.util.ArrayList;
 
 public class User {
-    private String userName, displayName, password, picture;
-    private ArrayList<Chat> chats ;
-    public User(String userName, String displayName, String password, String picture) {
+    private final String userName;
+    private final String displayName;
+    private final String password;
+    private final String defaultServerAdr;
+    private final ArrayList<Chat> chats;
+
+    public User(String userName, String displayName, String password) {
         this.userName = userName;
         this.displayName = displayName;
         this.password = password;
-        this.picture = picture;
         this.chats = new ArrayList<Chat>();
+        this.defaultServerAdr =  "10.0.2.2";
     }
 
     private void addChat(Chat chat) {
         chats.add(chat);
     }
 
-    private Chat searchChat(int id) {
+    private Chat searchChat(String userName) {
         for (Chat chat: this.chats) {
-            if (chat.getId() == id) {
+            if (chat.getUserName() == userName) {
                 return chat;
             }
         }
         return null;
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDefaultServerAdr() {
+        return defaultServerAdr;
+    }
+
+    public ArrayList<Chat> getChats() {
+        return chats;
     }
 /*
     private void sendMessage(String to, String text) {

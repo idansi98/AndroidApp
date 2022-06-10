ea138d4435b6f089a52d8c0517639a225e7e42f2
idansi98 <idansi98@gmail.com>
2022-06-10 12:13:19 +0300

    a

---
+++ app/src/main/java/com/example/androidapp/ChatsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats_list);
+    }
+}

0f81f3fe7b7e3a220904ddd85aaa8dbfd88a1ab9
idansi98 <idansi98@gmail.com>
2022-06-10 15:53:59 +0300

    a

--- app/src/main/java/com/example/androidapp/ChatsListActivity.java
+++ app/src/main/java/com/example/androidapp/ChatsListActivity.java
@@ -3,6 +3,14 @@ package com.example.androidapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidapp.adapters.ChatsListAdapter;
+import com.example.androidapp.classes.Chat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatsListActivity extends AppCompatActivity {
 
@@ -10,5 +18,14 @@ public class ChatsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats_list);
+        RecyclerView chatslist = findViewById(R.id.chatslist);
+        ChatsListAdapter adapter = new ChatsListAdapter(this);
+        chatslist.setAdapter(adapter);
+        chatslist.setLayoutManager(new LinearLayoutManager(this));
+        List<Chat> chats = new ArrayList<>();
+        chats.add(new Chat("Idan Simai", "IS","Null"));
+        chats.add(new Chat("Ido Tziony", "IT","Null"));
+        chats.add(new Chat("Hemi Leibo", "HL","Null"));
+        adapter.setChats(chats);
     }
 }
---
+++ app/src/main/java/com/example/androidapp/adapters/ChatsListAdapter.java
@@ -0,0 +1,75 @@
+package com.example.androidapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidapp.R;
+import com.example.androidapp.classes.Chat;
+
+import java.util.List;
+
+public class ChatsListAdapter extends RecyclerView.Adapter<ChatsListAdapter.ViewHolder> {
+    private List<Chat> chats;
+    private LayoutInflater lif;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+
+    public ChatsListAdapter(Context context) {
+        this.lif = LayoutInflater.from(context);
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = lif.inflate(R.layout.chatitem, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        if (chats != null ) {
+            return chats.size();
+        }
+        return 0;
+
+    public void setChats(List<Chat> s) {
+        this.chats = s;
+        notifyDataSetChanged();
+    }
+}
+

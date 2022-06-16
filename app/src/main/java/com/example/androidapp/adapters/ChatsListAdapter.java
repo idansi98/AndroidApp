1566332ac6cf63333cff6fd197faaaf9d557638f
idansi98 <idansi98@gmail.com>
2022-06-16 23:43:00 +0300

    sa

--- app/src/main/java/com/example/androidapp/adapters/ChatsListAdapter.java
+++ app/src/main/java/com/example/androidapp/adapters/ChatsListAdapter.java
@@ -8,9 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.androidapp.ChatActivity;
 import com.example.androidapp.R;
 import com.example.androidapp.classes.Chat;

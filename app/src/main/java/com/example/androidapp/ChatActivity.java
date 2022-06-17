14d4b1394671c87d36c1111c749cda0acbad919e
idansi98 <idansi98@gmail.com>
2022-06-17 14:27:44 +0300

    ס

--- app/src/main/java/com/example/androidapp/ChatActivity.java
+++ app/src/main/java/com/example/androidapp/ChatActivity.java
@@ -3,8 +3,13 @@ package com.example.androidapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
+import com.example.androidapp.adapters.ChatAdapter;
+import com.example.androidapp.adapters.ChatsListAdapter;
+import com.example.androidapp.api.ContactApi;
 import com.example.androidapp.classes.AppDB;
 import com.example.androidapp.classes.ChatDao;
 import com.example.androidapp.classes.MessageDao;
@@ -27,5 +32,20 @@ public class ChatActivity extends AppCompatActivity {
         chatDao = db.chatDao();
         messageDao = db.messageDao();
 
+
+
+        RecyclerView chat = findViewById(R.id.chatRecyclerView);
+        final ChatAdapter adapter = new ChatAdapter(this);
+        chat.setAdapter(adapter);
+        chat.setLayoutManager(new LinearLayoutManager(this));
+        ContactApi contactApi = new ContactApi(userDao, chatDao, messageDao);
+        Thread thread = new Thread(){
+            public void run(){
+                contactApi.get();
+            }
+        };
+        thread.start();
+        adapter.setMessageDao(messageDao);
+        adapter.setMessages(messageDao.index());
     }
 }
